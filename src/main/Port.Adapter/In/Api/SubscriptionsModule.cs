fee2540fbc0c8f48d38c84e43ddf4b60663b9083
Junvic <contact@junvic.me>
2022-08-30 03:50:18 +0800

    Support for generic subscriptions (#22)
    * refactor: support generic subscription requests
    
    * refactor: create concrete impl of generic command handler to workaround CQRSlite
    
    * refactor: rename UserId to UserNeuronId

--- src/main/Port.Adapter/In/Api/SubscriptionsModule.cs
+++ src/main/Port.Adapter/In/Api/SubscriptionsModule.cs
@@ -1,46 +1,59 @@
 ﻿using CQRSlite.Commands;
-using ei8.Cortex.Diary.Nucleus.Application;
 using ei8.Cortex.Diary.Nucleus.Application.Subscriptions.Commands;
 using ei8.Cortex.Subscriptions.Common;
+using ei8.Cortex.Subscriptions.Common.Receivers;
 using Nancy;
 using neurUL.Common.Api;
 using System;
+using System.Threading.Tasks;
 
 namespace ei8.Cortex.Diary.Nucleus.Port.Adapter.In.Api
 {
     public class SubscriptionsModule : NancyModule
     {
-        public SubscriptionsModule(ICommandSender commandSender,
-            ISettingsService settings) : base("/nuclei/d23/subscriptions")
+        public SubscriptionsModule(ICommandSender commandSender) : base("/nuclei/d23/subscriptions")
         {
-            this.Post("/", async (parameters) =>
+            this.Post("/receivers/{receiverType}", async (parameters) =>
             {
                 return await this.Request.ProcessCommand(
                         false,
                         async (bodyAsObject, bodyAsDictionary, expectedVersion) =>
                         {
-                            var subscriptionInfo = new BrowserSubscriptionInfo()
-                            {
-                                UserId = Guid.Parse(bodyAsObject.UserId.ToString()),
-                                AvatarUrl = bodyAsObject.AvatarUrl,
-                                Name = bodyAsObject.Name,
-                                PushEndpoint = bodyAsObject.PushEndpoint,
-                                PushAuth = bodyAsObject.PushAuth,
-                                PushP256DH = bodyAsObject.PushP256DH,
-                            };
-
-                            await commandSender.Send(new AddSubscription(subscriptionInfo, expectedVersion));
+                            await SendGenericRequestAsync(commandSender, bodyAsObject, parameters.receiverType, expectedVersion);
                         },
                         NeuronModule.ConcurrencyExceptionSetter,
                         new string[0],
-                        "AvatarUrl",
-                        "Name",
-                        "PushAuth",
-                        "PushP256DH",
-                        "PushEndpoint",
+                        "SubscriptionInfo",
+                        "ReceiverInfo",
                         "UserId"
                         );
             });
+        }
+
+        private async Task SendGenericRequestAsync(ICommandSender commandSender, dynamic bodyAsObject, string receiverType, int expectedVersion)
+        {
+            var subscriptionInfo = new SubscriptionInfo()
+            {
+                UserNeuronId = Guid.Parse(bodyAsObject.UserId.ToString()),
+                AvatarUrl = bodyAsObject.SubscriptionInfo.AvatarUrl,
+            };
+
+            switch (receiverType)
+            {
+                case "web":
+                    var receiverInfo = new BrowserReceiverInfo()
+                    {
+                        Name = bodyAsObject.ReceiverInfo.Name,
+                        PushAuth = bodyAsObject.ReceiverInfo.PushAuth,
+                        PushEndpoint = bodyAsObject.ReceiverInfo.PushEndpoint,
+                        PushP256DH = bodyAsObject.ReceiverInfo.PushP256DH,
+                    };
+                    await commandSender.Send(new AddSubscription<BrowserReceiverInfo>(subscriptionInfo, receiverInfo, expectedVersion));
+                    break;
+
+                default:
+                    throw new NotSupportedException($"Unsupported receiver type for endpoint {receiverType}");
+            }
         }
     }
 }
